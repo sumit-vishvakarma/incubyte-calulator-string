f6e0166765df59b9efcd1b6398912b61f7afe835
sumit-vishvakarma <sumit.vishwa1999@gmail.com>
2024-05-28 16:03:42 +0530

    wrote test case for add method

--- calculator_string.rb
+++ calculator_string.rb
@@ -1,2 +1,13 @@
+require 'pry-nav'
+
 class CalculatorString
+	def self.add(value)
+    return 0 if value.empty?
+
+    if value == "1"
+      return 1
+    elsif value[0].to_i > 0 && value[2].to_i > 0 && value[1] == ','
+			return value[0].to_i + value[2].to_i
+    end
+  end
 end
--- spec/calculator_string_spec.rb
+++ spec/calculator_string_spec.rb
@@ -1,4 +1,21 @@
 require_relative "../calculator_string"
 
 RSpec.describe CalculatorString do
+	describe "to match data" do
+    context "given an empty string" do
+      it "returns 0" do
+        expect(CalculatorString.add("")).to eq(0)
+      end
+    end
+		context "given an single value" do	
+      it "returns value " do
+        expect(CalculatorString.add("1")).to eq(1)
+      end
+    end
+		context "addition integer" do
+      it "returns addition of value" do
+        expect(CalculatorString.add("1,2")).to eq(3)
+      end
+    end
+	end
 end

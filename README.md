6316fdd4f215c035facabd8e1fb3e2ce78af9470
sumit-vishvakarma <sumit.vishwa1999@gmail.com>
2024-05-28 20:09:00 +0530

    README.md file

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# incubyte-calulator-string
+StringCalculator
+
+The StringCalculator class is a Ruby implementation designed to perform arithmetic operations on strings of numbers. It can handle custom delimiters, ignore numbers larger than 1000, and disallow negative numbers.
+
+Features
+
+- Basic Addition: Compute the sum of numbers separated by commas or newlines.
+- Custom Delimiters: Support for single-character and multi-character custom delimiters.
+- Negatives Not Allowed: Throws an exception if the input contains negative numbers.
+- Ignore Large Numbers: Numbers larger than 1000 are ignored during sum calculation.
+- Invocation Count: Keeps track of how many times the add method has been called.
+
+Running Tests
+
+This project uses RSpec for testing. To run the tests, first, make sure you have RSpec installed:
+
+```gem install rspec```
+
+Then, navigate to the project directory and run:
+
+```rspec```
+
+This will execute the tests defined in string_calculator_spec.rb, verifying the functionality of the StringCalculator.
--- spec/calculator_string_spec.rb
+++ spec/calculator_string_spec.rb
@@ -7,31 +7,37 @@ RSpec.describe CalculatorString do
         expect(CalculatorString.add("")).to eq(0)
       end
     end
+
 		context "given an single value" do	
       it "returns value " do
         expect(CalculatorString.add("1")).to eq(1)
       end
     end
+
 		context "addition integer" do
       it "returns addition of value" do
         expect(CalculatorString.add("1,2")).to eq(3)
       end
     end
+
     context "addition of multiple values" do
       it "add multiple value" do
         expect(CalculatorString.add("1,2,4,5,5")).to eq(17)
       end
     end
+
     context "handle new lines between the number" do
       it "handle new lines (instead of commas)" do
         expect(CalculatorString.add("1\n2,3,8")).to eq(14)
       end
     end
+
     context "handle non negative value" do
       it "raises an error for negative numbers" do
         expect { CalculatorString.add("-1") }.to raise_error("negative numbers not allowed please give positive number -1")
       end
     end
+
     context "handle custom delimiter" do
       it "handles custom delimiter and adds the integers" do
         expect(CalculatorString.add("//;\n1;2")).to eq(3)
